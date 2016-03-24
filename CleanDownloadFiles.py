c94575720bccd9c25cf4d04b3d077c2bc6ecdcfa
Nicolas Bigler <bigler.nicolas@gmail.com>
2016-03-24 07:50:09 +0100

    Added a python script to clean the directories (make sure that there is never more than 20 files in them. Need to be tested.

---
+++ CleanDownloadFiles.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+
+import os
+import pdb
+
+current_directory = '/home/nico/Documents/projects/workingFiles/podcastRasbPi/podcasts'
+dir_to_clean = ['Forum-La1?re', 'LeJournalhoraire-La1?re', 'Lalibrairiefrancophone-La1?re']
+
+for item in dir_to_clean:
+    dir_curr = current_directory + os.sep + item
+    files = os.listdir(dir_curr)
+    files = sorted(files)
+
+    while files.__len__() > 20:
+        os.remove(dir_curr + os.sep + files[0])
+        del files[0]
+        
+
