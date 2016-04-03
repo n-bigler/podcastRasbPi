c6d837188d6815c5c82896b5915beeebe3e23cf7
Nicolas Bigler <bigler.nicolas@gmail.com>
2016-04-03 15:25:47 +0200

    Softcoded the current directory in CleanDownloadFiles.py

--- CleanDownloadFiles.py
+++ CleanDownloadFiles.py
@@ -4,7 +4,7 @@
 import os
 import pdb
 
-current_directory = '/home/pi/Documents/projects/podcastRasbPi/podcasts'
+current_directory = os.getcwd()
 dir_to_clean = ['Forum-La1?re', 'LeJournalhoraire-La1?re', 'Lalibrairiefrancophone-La1?re']
 
 for item in dir_to_clean:
